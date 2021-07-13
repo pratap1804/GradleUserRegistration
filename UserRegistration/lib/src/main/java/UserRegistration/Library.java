3b298f6f54e137d5dd257879dd30d9c8f399f667
pratap1804 <pratap.1804@gmail.com>
2021-07-13 22:30:00 +0530

    Added: Welcome message

---
+++ UserRegistration/lib/src/main/java/UserRegistration/Library.java
@@ -0,0 +1,9 @@
+package UserRegistration;
+
+public class Library {
+	
+	
+    public static void main(String[] args) {
+        System.out.println("Welcome here!");
+    }
+}
---
+++ UserRegistration/lib/src/test/java/UserRegistration/LibraryTest.java
@@ -0,0 +1,10 @@
+package UserRegistration;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+    @Test void someLibraryMethodReturnsTrue() {
+ 
+    }
+}
